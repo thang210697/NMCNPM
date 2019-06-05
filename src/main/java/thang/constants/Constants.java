cf94f672dc490b0a3dfe8944cf4f3cfe4a50ab52
Nguyễn Văn Trọng <Nguyentrongnguyen2611@gmail.com>
2019-06-05 15:45:25 +0700

    Update Constants.java
    add comment

--- src/main/java/thang/constants/Constants.java
+++ src/main/java/thang/constants/Constants.java
@@ -1,19 +1,18 @@
-package thang.constants;
+﻿package thang.constants;
 
+import nmcnpm.constants.String;
+
+//Dùng để lưu client_id, client_secret, redirect_uri
 public class Constants {
-	  public static String FACEBOOK_APP_ID = "965814963626813";
+	public static String GOOGLE_CLIENT_ID = "803901185756-e0qa3h5hicn9botoesnieba078mufu9s.apps.googleusercontent.com";
+	public static String GOOGLE_CLIENT_SECRET = "Km_gh-vgyqzuYQPkKAazGejJ";
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/thang/action?action=logingg";
+	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+
+	public static String FACEBOOK_APP_ID = "965814963626813";
 	  public static String FACEBOOK_APP_SECRET = "e8944b4292d141aba0c87257eb3c79c0";
 	  public static String FACEBOOK_REDIRECT_URL = "https://localhost:8443/thang/action?action=loginfb";
 	  public static String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&redirect_uri=%s&code=%s";
-	
-
-
-	  public static String GOOGLE_CLIENT_ID = "803901185756-e0qa3h5hicn9botoesnieba078mufu9s.apps.googleusercontent.com";
-	  public static String GOOGLE_CLIENT_SECRET = "Km_gh-vgyqzuYQPkKAazGejJ";
-	  public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/thang/action?action=logingg";
-	  public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
-	  public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
-	  public static String GOOGLE_GRANT_TYPE = "authorization_code";
-
-
 }
